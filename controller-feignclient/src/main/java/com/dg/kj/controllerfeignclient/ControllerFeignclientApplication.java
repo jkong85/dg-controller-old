1104e91b76d5a94364250fc246b0ce7f0ea0b0bb
Jian <kongjian.work@gmail.com>
2018-07-09 01:06:48 -0500

    test httpclient todo: pass the json, and test create pod in k8s

--- controller-feignclient/src/main/java/com/dg/kj/controllerfeignclient/ControllerFeignclientApplication.java
+++ controller-feignclient/src/main/java/com/dg/kj/controllerfeignclient/ControllerFeignclientApplication.java
@@ -4,6 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
+import java.util.HashMap;
+import java.util.Map;
 
 @SpringBootApplication
 @EnableFeignClients
@@ -11,5 +17,21 @@ public class ControllerFeignclientApplication extends SpringBootServletInitializ
 
     public static void main(String[] args) {
         SpringApplication.run(ControllerFeignclientApplication.class, args);
+        ControllerFeignclientApplication instance = new ControllerFeignclientApplication();
+        instance.getK8sApiServer();
+    }
+
+    public String getK8sApiServer(){
+        Map<String,String> map=new HashMap<String,String>();
+        map.put("name","123");
+        map.put("password","123");
+        URI uri = UriComponentsBuilder.fromHttpUrl("http://localhost:8001/")
+//                .queryParam("jsonString",JSON.toJSONString(map))
+//                .queryParam("token","12122222111")
+                .build().encode().toUri();
+        RestTemplate restTemplate=new RestTemplate();
+        String data=restTemplate.getForObject(uri,String.class);
+        System.out.println(data);
+        return null;
     }
 }
--- controller-kubeclient/src/main/java/com/dg/kj/controllerkubeclient/ControllerKubeclientApplication.java
+++ controller-kubeclient/src/main/java/com/dg/kj/controllerkubeclient/ControllerKubeclientApplication.java
@@ -1,44 +1,60 @@
 package com.dg.kj.controllerkubeclient;
 
-import io.kubernetes.client.ApiClient;
+import com.alibaba.fastjson.JSON;
 import io.kubernetes.client.ApiException;
-import io.kubernetes.client.Configuration;
-import io.kubernetes.client.util.Config;
-import io.kubernetes.client.apis.CoreV1Api;
-import io.kubernetes.client.models.V1Pod;
-import io.kubernetes.client.models.V1PodList;
 import java.io.IOException;
+import java.net.URI;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 public class ControllerKubeclientApplication {
-    private static DevK8sApiService k8sApiService;  // Fabric8
-    private static ApiClient k8sApiClient;
+    private String urlApiServer = "http://localhost:8001/";
     public static void main(String[] args)throws IOException, ApiException {
-        //SpringApplication.run(ControllerKubeclientApplication.class, args);
+        SpringApplication.run(ControllerKubeclientApplication.class, args);
+        ControllerKubeclientApplication client = new ControllerKubeclientApplication();
+        client.getK8sApiServer();
+    }
 
-        // For Fabirc Java Client
-//        k8sApiService = new DevK8sApiService();
-//        k8sApiService.init();
-//        k8sApiService.listNamespace();
-//        k8sApiService.listNode();
+    public String getK8sApiServer(){
+        Map<String,String> map=new HashMap<String,String>();
+        map.put("name","123");
+        map.put("password","123");
+        URI uri = UriComponentsBuilder.fromHttpUrl(urlApiServer)
+//                .queryParam("jsonString",JSON.toJSONString(map))
+                .build().encode().toUri();
+        RestTemplate restTemplate=new RestTemplate();
+        String data=restTemplate.getForObject(uri,String.class);
+        System.out.println(data);
+        return null;
+    }
+    public String createPod(){
+        String url = urlApiServer;
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
 
-//        ApiClient client = Config.defaultClient();
-//        Configuration.setDefaultApiClient(client);
-//
-//
-//        CoreV1Api api = new CoreV1Api();
-//        V1PodList list = api.listPodForAllNamespaces(null, null, null, null, null, null, null, null, null);
-//        for (V1Pod item : list.getItems()) {
-//            System.out.println(item.getMetadata().getName());
-//        }
+        MultiValueMap<String, Object> params = new LinkedMultiValueMap<>();
+        params.add("logo", new FileSystemResource("C:\\Users\\lixiangke\\Pictures\\Saved Pictures\\jdsadh.jpg"));
+        params.add("nickname", "nick");
 
-//        k8sApiClient = Config.defaultClient();
-//        Configuration.setDefaultApClient(k8sApiClient);
+        HttpEntity<MultiValueMap<String, Object>> httpEntity = new HttpEntity<>(params, headers);
+        RestTemplate restTemplate = new RestTemplate();
+        String str = restTemplate.postForObject(url, httpEntity, String.class);
+        System.out.println(str);
 
+        return null;
     }
 }
