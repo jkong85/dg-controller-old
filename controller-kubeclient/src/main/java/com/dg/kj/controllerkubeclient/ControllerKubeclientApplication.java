73033664200ff6a810bcff666906cb8ca1a48d8b
Jian <kongjian.work@gmail.com>
2018-07-10 14:11:30 -0600

    extensionv1API

--- controller-kubeclient/src/main/java/com/dg/kj/controllerkubeclient/ControllerKubeclientApplication.java
+++ controller-kubeclient/src/main/java/com/dg/kj/controllerkubeclient/ControllerKubeclientApplication.java
@@ -12,6 +12,7 @@ import java.util.Map;
 
 import io.kubernetes.client.Configuration;
 import io.kubernetes.client.apis.CoreV1Api;
+import io.kubernetes.client.apis.ExtensionsV1beta1Api;
 import io.kubernetes.client.models.*;
 import io.kubernetes.client.util.Config;
 import org.springframework.boot.SpringApplication;
@@ -86,17 +87,17 @@ spec:
         kubernetes.io/hostname: docker-for-desktop
          */
         V1Deployment deploy = new V1Deployment();
-        deploy.apiVersion("app/v1");
+        deploy.setApiVersion("app/v1");
         V1ObjectMeta deployMeta = new V1ObjectMeta();
-        deployMeta.name("controller-test");
+        deployMeta.setName("controller-test");
         deploy.setMetadata(deployMeta);
         V1DeploymentSpec deploySpec = new V1DeploymentSpec();
-        deploySpec.setReplicas(1);
         V1LabelSelector labelSelector = new V1LabelSelector();
         Map<String, String> labelMap = new HashMap<>();
         labelMap.put("app", "controller");
         labelSelector.matchLabels(labelMap);
         deploySpec.setSelector(labelSelector);
+        deploySpec.setReplicas(1);
         V1PodTemplateSpec template = new V1PodTemplateSpec();
         V1ObjectMeta template_metadata = new V1ObjectMeta();
         Map<String, String> template_label_map = new HashMap<>();
@@ -119,16 +120,74 @@ spec:
         port1.setContainerPort(9005);
         listPort.add(port1);
         container1.setPorts(listPort);
-        podSpec.containers(listContainer);
-        //
+        podSpec.setContainers(listContainer);
         Map<String, String> nodeSel = new HashMap<>();
         nodeSel.put("kubernetes.io/hostname", "docker-for-desktop");
-        podSpec.nodeSelector(nodeSel);
-        template.spec(podSpec);
-        template.metadata(template_metadata);
-        deploySpec.template(template);
+        podSpec.setNodeSelector(nodeSel);
+        template.setSpec(podSpec);
+        template.setMetadata(template_metadata);
+        deploySpec.setTemplate(template);
+        deploy.setSpec(deploySpec);
+
+
+        createDeployment();
 
         return null;
+    }
+
+    public void createDeployment() throws ApiException{
+
+        ExtensionsV1beta1Api extensionsV1beta1Api = new ExtensionsV1beta1Api();
+
+        //V1Deployment deploy = new V1Deployment();
+        ExtensionsV1beta1Deployment deploy = new ExtensionsV1beta1Deployment();
+        deploy.setApiVersion("extensions/v1beta1");
+        V1ObjectMeta deployMeta = new V1ObjectMeta();
+        deployMeta.setName("controller-test");
+        deploy.setMetadata(deployMeta);
+        //V1DeploymentSpec deploySpec = new V1DeploymentSpec();
+        ExtensionsV1beta1DeploymentSpec deploySpec = new ExtensionsV1beta1DeploymentSpec();
+        V1LabelSelector labelSelector = new V1LabelSelector();
+        Map<String, String> labelMap = new HashMap<>();
+        labelMap.put("app", "controller");
+        labelSelector.matchLabels(labelMap);
+        deploySpec.setSelector(labelSelector);
+        deploySpec.setReplicas(1);
+        V1PodTemplateSpec template = new V1PodTemplateSpec();
+        V1ObjectMeta template_metadata = new V1ObjectMeta();
+        Map<String, String> template_label_map = new HashMap<>();
+        template_label_map.put("app", "controller");
+        template_metadata.labels(template_label_map);
+        V1PodSpec podSpec = new V1PodSpec();
+        List<V1Container> listContainer = new ArrayList<>();
+        V1Container container1= new V1Container();
+        container1.setName("controller-test");
+        container1.setImage("jkong85/dg-controller-test:0.1");
+        List<V1EnvVar> listEnv = new ArrayList<>();
+        V1EnvVar env1 = new V1EnvVar();
+        env1.setName("EUREKA_SERVER_IP");
+        env1.setValue("10.1.0.78");
+        listEnv.add(env1);
+        container1.setEnv(listEnv);
+        listContainer.add(container1);
+        List<V1ContainerPort> listPort = new ArrayList<>();
+        V1ContainerPort port1 = new V1ContainerPort();
+        port1.setContainerPort(9005);
+        listPort.add(port1);
+        container1.setPorts(listPort);
+        podSpec.setContainers(listContainer);
+        Map<String, String> nodeSel = new HashMap<>();
+        nodeSel.put("kubernetes.io/hostname", "docker-for-desktop");
+        podSpec.setNodeSelector(nodeSel);
+        template.setSpec(podSpec);
+        template.setMetadata(template_metadata);
+        deploySpec.setTemplate(template);
+        deploy.setSpec(deploySpec);
+
+        //
+        System.out.println("Create deployment with API server");
+        extensionsV1beta1Api.createNamespacedDeployment("default", deploy, null);
+
     }
     public String createPod(){
         String url = urlApiServer;
