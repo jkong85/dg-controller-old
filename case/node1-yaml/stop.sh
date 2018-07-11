bf414925394e4aada8433e027c2062f5b27f4cb9
jian <kongjian.work@gmail.com>
2018-07-11 17:03:22 +0000

    stop.sh

--- case/node1-yaml/stop.sh
+++ case/node1-yaml/stop.sh
@@ -1,12 +1,11 @@
 #!/bin/sh
 
 kubectl delete -f eureka.yaml
-sleep 5 
 
 kubectl delete -f test.yaml
-sleep 5 
 
 kubectl delete -f zuul.yaml
-sleep 5 
 
 kubectl delete -f service.yaml
+
+kubectl delete -f kubeclient.yaml 
